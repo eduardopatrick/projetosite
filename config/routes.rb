039021e585ade5e3fe066c95479c8d62be76a6d1
Gleidson Silva <gleidson.fgs@gmail.com>
2017-12-05 14:58:48 -0300

    corrigido erro de login, exclusão do User

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
- resources :posts 
-
+  # devise_for :users
+ resources :posts
  root 'posts#index'
 end
