1718097af2539cfa972ee7f82e0fdb886d1d00f3
Gleidson Silva <gleidson.fgs@gmail.com>
2017-12-05 10:14:37 -0300

    Alterações novas e correção de bugs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery-3.2.1.min
+// = require bootstrap
+//= require index
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-//= require bootstrap
-//= require index
